184aeb8a4eb71f342ac57f5ea4e4bd111c887d11
Caleb Butler <butlercq@gmail.com>
2024-05-15 11:28:48 -0400

    Create an initial spring project

---
+++ src/test/java/com/skillstorm/budgetbuddyaccountservice/BudgetBuddyAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.budgetbuddyaccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBuddyAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
