e2287faf54a0f8552d0fd9c33161e006c3788049
Caleb Butler <butlercq@gmail.com>
2024-05-15 12:03:32 -0400

    Add possible schema for database

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS accounts;
+
+CREATE TYPE ACCOUNT_TYPE AS ENUM ('CHECKING', 'SAVINGS', 'CREDIT', 'INVESTMENT');
+
+CREATE TABLE accounts (
+  id SERIAL PRIMARY KEY,
+  user_id VARCHAR(50),
+  _type ACCOUNT_TYPE,
+  account_number VARCHAR(50),
+  routing_number VARCHAR(50),
+  institution VARCHAR(50),
+  investment_rate DECIMAL,
+  starting_balance DECIMAL
+);
